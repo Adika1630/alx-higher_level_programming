235cc10425e5f0fef4dcef3befda46bf9af9719a
Adika1630 <addisu05mulat@gmail.com>
2023-03-13 06:58:55 -0700

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    y = 0
+    if my_list == None:
+        return None
+    for x in my_list:
+        if y == 0 or y < x:
+            y = x
+    return y
