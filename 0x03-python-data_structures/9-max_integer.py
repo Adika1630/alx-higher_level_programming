7878d9436518ab5738e710299bbb379a3f2ec728
Adika1630 <addisu05mulat@gmail.com>
2023-03-13 07:02:09 -0700

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     y = 0
-    if my_list == None:
+    if my_list is None:
         return None
     for x in my_list:
         if y == 0 or y < x:
